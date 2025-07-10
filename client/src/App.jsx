652eca68970a305ab341c908fee1a948c850a894
Rohit <rohhit.rz@gmail.com>
2025-07-10 18:11:08 +0530

    implemented sidebar and chat container section

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
