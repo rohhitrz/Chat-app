9b077aeb5f80c5400585e001950dc500e9494fec
Rohit <rohhit.rz@gmail.com>
2025-08-10 17:57:12 +0530

    fix

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -3,27 +3,25 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const protectRoute=async(req,res,next)=>{
-    try{
-        const token=req.headers.token;
-        const decoded=jwt.verify(token,process.env.SECRET);
-        const user=await User.findById(decoded.userId).select("-password");
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
 
-        if(!user){
-            return res.json({
-                success:false,
-                message:"user not found"
-            })
-        }
-        req.user=user;
-        next();
-    }catch(err){
-        console.log(err.message);
-        return res.json({
-            success:false,
-            message:err.message
-        })
-
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "user not found",
+      });
     }
-
-}
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err.message);
+    return res.json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
